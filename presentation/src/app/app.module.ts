75ca242deae0472f866e5147dff843b395787ea2
Dillon Stock <stockdillon@yahoo.com>
2019-06-22 15:40:46 -0400

    app.module.ts clean up

--- presentation/src/app/app.module.ts
+++ presentation/src/app/app.module.ts
@@ -6,19 +6,10 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import {
-  // MatInputModule,
-  MatButtonModule,
-  // MatCardModule,
-  // MatFormFieldModule,
-  // MatTableModule,
-  MatSidenavModule,
-  // MatRadioModule,
-  // MatDialogModule,
-  // MatSlideToggleModule,
-  MatCommonModule,
-  // MatCheckboxModule,
-  MatIconModule,
-  // MatMenuModule
+          MatButtonModule,
+          MatSidenavModule,
+          MatCommonModule,
+          MatIconModule,
         } from '@angular/material';
 import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
 import {MatListModule} from '@angular/material/list';
@@ -27,8 +18,6 @@ import {MatListModule} from '@angular/material/list';
 import { AppRoutingModule } from './app-routing.module';
 import {APP_BASE_HREF} from '@angular/common';
 import {MatToolbarModule} from '@angular/material/toolbar';
-// import { BlogComponent } from './pages/blog/blog.component';
-// import { HomeComponent } from './pages/home/home.component';
 import { PagesModule } from './pages/pages.module';
 import { NavModule } from './nav/nav.module';
 
@@ -46,23 +35,11 @@ import { ComponentsModule } from './components/components.module';
     AppRoutingModule,
     BrowserAnimationsModule,
     MatCommonModule,
-    // MatTableModule,
-    // MatCardModule,
-    // MatInputModule,
-    // MatFormFieldModule,
     MatButtonModule,
     MatToolbarModule,
     MatSidenavModule,
-    // MatRadioModule,
-    // MatDialogModule,
-    // MatSlideToggleModule,
-    // MatCheckboxModule,
     MatIconModule,
     MatListModule,
-    // MatMenuModule,
-    // MatProgressSpinnerModule,
-    // HttpClientModule,
-    // BlogPostsService,
     PagesModule,
     NavModule,
     ComponentsModule,
