555a4e8ad63dfe30b99651b53ed385e6b1eeb0f2
Dillon Stock <stockdillon@yahoo.com>
2021-08-08 00:01:58 -0400

    set baseUrl depending on prod/non-prod environment

--- presentation/src/environments/environment.ts
+++ presentation/src/environments/environment.ts
@@ -1,9 +1,11 @@
 // This file can be replaced during build by using the `fileReplacements` array.
 // `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
+import { Environment } from './environment.interface';
 
-export const environment = {
-  production: false
+export const environment: Environment = {
+  production: false,
+  baseUrl: 'http://dillonstock.com'
 };
 
 /*
